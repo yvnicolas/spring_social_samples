bae562d3c673e5a76f321d583f05aef6ee90f50c
Yves Nicolas <yves.nicolas@dynamease.com>
2013-09-17 11:22:44 +0200

    Initial Creation of background spring social sandbox

---
+++ background-spring-social/src/main/java/background-spring-social/App.java
@@ -0,0 +1,13 @@
+package background-spring-social;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
